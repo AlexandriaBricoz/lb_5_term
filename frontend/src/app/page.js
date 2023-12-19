aa3f6da8869d17413acf90b79f725b028a4e02cb
nerdermind <mansur.tumashev@gmail.com>
2023-12-19 14:07:03 +0500

    frontend

---
+++ frontend/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Graph from '@/components/graph'
+import React from "react";
+
+export default function Home() {
+  return (
+      <main className="bg-black">
+          <Graph/>
+      </main>
+  )
+}
